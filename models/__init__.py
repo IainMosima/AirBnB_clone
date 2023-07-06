ef6070ac1ed69963364a0755171155e2eb8f3910
IainMosima <iainmosima@gmail.com>
2023-07-06 12:04:58 +0300

    debugging

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,17 @@
-#!/bin/usr/python3
+#!/usr/bin/python3
 """
-Init for models module
+initialize the models package
 """
-from models.engine.file_storage import FileStorage
 
-storage = FileStorage()
+from os import getenv
+
+
+storage_t = getenv("HBNB_TYPE_STORAGE")
+
+if storage_t == "db":
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
 storage.reload()
